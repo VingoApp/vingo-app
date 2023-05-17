c2559131a696a9cc21c4419b045f828d23421366
alexandre-vl <alexandre.vargas.lopez@gmail.com>
2023-05-17 19:40:18 +0200

    dsfsd

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     legacy(),
     VitePWA({
+        includeAssets: ["favicon.png", "public/*"],
         registerType: 'autoUpdate',
 /*         devOptions: {
             enabled: true
